efcd899bae177841d42be65cc73074183c185a6f
kkahlua <csw93083@naver.com>
2022-03-29 23:53:00 +0900

    state 디자인 최종 결정

--- cctv_interface/src/components/State3.js
+++ cctv_interface/src/components/State3.js
@@ -1,6 +1,19 @@
 import React, { useState } from "react";
 import { Carousel } from "react-bootstrap";
+import styled from "styled-components";
 
+const StyledCarouselDiv = styled.div`
+  display: flex;
+  justify-items: center;
+  justify-content: center;
+  padding-bottom: 50px;
+  padding-top: 50px;
+  z-index: 1;
+  .active.carousel-item > img {
+    display: block;
+    width: 50rem;
+  }
+`;
 function State3() {
   const [index, setIndex] = useState(0);
 
@@ -9,49 +22,39 @@ function State3() {
   };
   return (
     <div className="State3">
-      <h1>상태3</h1>
-      <Carousel
-        activeIndex={index}
-        onSelect={handleSelect}
-        fade
-        interval={null}
-      >
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={require("../images/testImg4.jpg")}
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>여기에</h3>
-            <p>설명 적을 수 있음</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            alt="Second slide"
-            src={require("../images/testImg5.jpg")}
-          />
+      <h1>상태2</h1>
+      <StyledCarouselDiv>
+        <Carousel
+          activeIndex={index}
+          onSelect={handleSelect}
+          fade
+          interval={null}
+        >
+          <Carousel.Item>
+            <img src={require("../images/testImg4.jpg")} alt="First slide" />
+            <Carousel.Caption>
+              <h3>여기에</h3>
+              <p>설명 적을 수 있음</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img alt="Second slide" src={require("../images/testImg5.jpg")} />
 
-          <Carousel.Caption>
-            <h3>여기에</h3>
-            <p>설명 적을 수 있음</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={require("../images/testImg6.jpg")}
-            alt="Third slide"
-          />
+            <Carousel.Caption>
+              <h3>여기에</h3>
+              <p>설명 적을 수 있음</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={require("../images/testImg6.jpg")} alt="Third slide" />
 
-          <Carousel.Caption>
-            <h3>여기도 </h3>
-            <p>설명 적을 수 있음</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+            <Carousel.Caption>
+              <h3>여기도 </h3>
+              <p>설명 적을 수 있음</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </StyledCarouselDiv>
     </div>
   );
 }
